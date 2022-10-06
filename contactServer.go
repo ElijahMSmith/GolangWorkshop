1bede77ba2f810a2e2341ee1cef6c3c9d23158ae
Elijah Smith <mcditto99@gmail.com>
2022-10-05 20:55:43 -0400

    Setup contact server

--- contactServer.go
+++ contactServer.go
@@ -1,3 +1,15 @@
 package main
 
-// Show that you can also run this through Cobra as a command line utility
+import "github.com/gin-gonic/gin"
+
+func newContact(c *gin.Context) {
+	// TODO
+}
+
+func getContact(c *gin.Context) {
+	// TODO
+}
+
+func deleteContact(c *gin.Context) {
+	// TODO
+}
